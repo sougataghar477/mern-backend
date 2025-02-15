d9ace82633e71794a54735b0f59d46ec9ba257c6
sougata <sougataghar47@gmail.com>
2025-02-15 22:36:56 +0530

    2nd push

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ app.use(
     credentials: true,
   })
 );
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://mern-frontend-chi-taupe.vercel.app"); // ✅ Replace with your frontend URL
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true"); // ✅ Required for cookies
+    next();
+  });
+  
 app.use(express.json());
 
 app.use(
